413fa14a592b69db500e7bba9c8287be1d832f70
Md. Iqbal Hasan <140314457+Minhaz053@users.noreply.github.com>
2023-07-30 16:28:34 +0600

    Add files via upload

---
+++ Lab 01/Minhaz 01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Welcome to the wonderful world of c++!!!\n";
+    cout<<"Lab1\t";
+    cout<<"\n END\tPROGRAM\tBYE";
+    cout<<"Welcome in yarmouk university\rHello";
+
+
+
+
+}
