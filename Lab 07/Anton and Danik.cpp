586361bd9722cb5751b2635d556ef977be970d9a
Md. Iqbal Hasan <140314457+Minhaz053@users.noreply.github.com>
2023-10-15 13:00:15 +0600

    Create Anton and Danik.cpp

---
+++ Lab 07/Anton and Danik.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int n,a=0,d=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='A')
+        {
+            a++;
+        }
+        else
+            d++;
+    }
+
+    if(a>d)
+        cout<<"Anton"<<endl;
+    else if(a<d)
+        cout<<"Danik"<<endl;
+    else
+        cout<<"Friendship"<<endl;
+
+}
